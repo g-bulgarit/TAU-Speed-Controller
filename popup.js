4fd669e13ef78038bb20ed7fa7389ad7e5db9aa9
g-bulgarit <alonio6@gmail.com>
2020-03-14 22:07:17 +0200

    added more buttons and more options

--- popup.js
+++ popup.js
@@ -1,28 +1,51 @@
+/* Gross functions */
 
-function go_faster(){
-  var current_playback_rate;
-  //current_playback_rate = document.getElementsByTagName('video')[0].playbackRate;
+function speed_15(){
   chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
     chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 1.5;"});
-});
-}
+});}
 
-function go_slower(){
-  var current_playback_rate;
-  //current_playback_rate = document.getElementsByTagName('video')[0].playbackRate;
+function speed_125(){
   chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 0.5;"});
+    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 1.25;"});
+});}
+
+
+function speed_normal(){
+  chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 1;"});
+});}
+
+function speed_85(){
+  chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 0.85;"});
+});}
+
+function speed_75(){
+  chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 0.75;"});
+});}
+
+/* Gross event listeners */
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("fast15").addEventListener("click", speed_15);
 });
-  
-  
-}
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("faster_button").addEventListener("click", go_faster);
+  document.getElementById("fast125").addEventListener("click", speed_125);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("slower_button").addEventListener("click", go_slower);
+  document.getElementById("normal").addEventListener("click", speed_normal);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("slow85").addEventListener("click", speed_85);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("slow75").addEventListener("click", speed_75);
+});
+
 
 
