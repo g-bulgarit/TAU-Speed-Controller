bd86ed198fb55c5fbe991060801a735086c7e019
g-bulgarit <alonio6@gmail.com>
2020-03-15 14:00:18 +0200

    Moved from buttons to a slider with a label.
    Still barebones-kind-of-execution that needs more work.

--- popup.js
+++ popup.js
@@ -1,36 +1,27 @@
-/* Gross functions */
-
-function speed_15(){
-  chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 1.5;"});
-});}
-
-function speed_125(){
-  chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 1.25;"});
-});}
-
-
 function speed_normal(){
+  // Set video playback speed to 1x
+
   chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
     chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 1;"});
+    document.getElementById("slider").value = "1";
+    document.getElementById("slider_value").innerHTML = "1";
 });}
 
-function speed_85(){
+function update_slider(){
+  // Update slider label based on slider position,
+  // Set video playback rate to slider value.
+
+  var slider_value = document.getElementById("slider").value;
+  var action_str = "document.getElementsByTagName('video')[0].playbackRate =" + slider_value;
+  console.log(slider_value);
+  console.log(action_str);
+  document.getElementById("slider_value").innerHTML = document.getElementById("slider").value;
   chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 0.85;"});
-});}
+    chrome.tabs.executeScript(activeTabs[0].id, { code: action_str});})
+  }
 
-function speed_75(){
-  chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 0.75;"});
-});}
-
-/* Gross event listeners */
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("fast15").addEventListener("click", speed_15);
-  document.getElementById("fast125").addEventListener("click", speed_125);
-  document.getElementById("normal").addEventListener("click", speed_normal);
-  document.getElementById("slow85").addEventListener("click", speed_85);
-  document.getElementById("slow75").addEventListener("click", speed_75);
+  // Add an event listener for the slider and for the reset button
+  document.getElementById("reset").addEventListener("click", speed_normal);
+  document.getElementById("slider").addEventListener("change", update_slider);
 });
