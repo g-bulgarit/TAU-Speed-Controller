dc612c806e6c71c61417c6294e9d3a0e8b18f2ba
g-bulgarit <alonio6@gmail.com>
2020-03-26 21:17:29 +0200

    Added Zoom and TAU-Webserver support

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ function speed_normal(){
   
   chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
     chrome.tabs.executeScript(activeTabs[0].id, { code: "document.getElementsByTagName('video')[0].playbackRate = 1;"});
+    chrome.tabs.executeScript(activeTabs[0].id, { code: "document.querySelector('video').playbackRate = 1;"});
     document.getElementById("slider").value = "1";
     document.getElementById("slider_value").innerHTML = "1";
   });}
@@ -14,10 +15,12 @@ function update_slider(){
   // Set video playback rate to slider value.
 
   var slider_value = document.getElementById("slider").value;
-  var action_str = "document.getElementsByTagName('video')[0].playbackRate =" + slider_value;
+  var action_str_unicko = "document.getElementsByTagName('video')[0].playbackRate =" + slider_value;
+  var action_str_zoom = "document.querySelector('video').playbackRate =" + slider_value;
   document.getElementById("slider_value").innerHTML = document.getElementById("slider").value;
   chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-    chrome.tabs.executeScript(activeTabs[0].id, { code: action_str});})
+    chrome.tabs.executeScript(activeTabs[0].id, { code: action_str_unicko});
+    chrome.tabs.executeScript(activeTabs[0].id, { code: action_str_zoom});})
   }
 
 function update_number(){
